c134685b7bda8a3fa37259db9cb917304352f3b8
Amir Javed <amir.javed@hotmail.co.uk>
2020-04-21 20:27:29 +0100

    Redux added

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import Store from './Redux/store';
+
 import Landing from './Pages/Landing/Landing';
 import Auth from './Pages/Auth/Auth';
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/" component={Landing} exact />
-          <Route path="/auth" component={Auth} exact />
-          <Route path="/register" component={Auth} exact />
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={Store}>
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/" component={Landing} exact />
+            <Route path="/auth" component={Auth} exact />
+            <Route path="/register" component={Auth} exact />
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
