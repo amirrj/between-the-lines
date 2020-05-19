5276366d9fb2a3938ab306897b1cce7bdf0096df
Amir Javed <amir.javed@hotmail.co.uk>
2020-05-19 20:27:04 +0100

    added cors

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 const path = require('path');
-const cors = require('cors');
 
 const app = express();
 app.use(
@@ -11,7 +10,16 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
 
 const db = config.get('mongoURI');
 
